80c4292efd1fdb3c05845bc485fee54fd94145a1
Chimdindu Chukwuka <64972836+Chimdindu-dev@users.noreply.github.com>
2022-11-19 10:56:35 +0200

    changing seconds to integers

--- controllers/neatendata.py
+++ controllers/neatendata.py
@@ -1,32 +1,36 @@
 from SpeakerDiarization import *
+import numpy as np
 
-dataneed = SpeakerDiarization(audio="") #needs audio file url
+dataneed = SpeakerDiarization(audio="") #needs audio url
 
 transcription = dataneed["text"]
 diarization = dataneed["utterances"]
 audiolength = dataneed["audio_duration"]
 
 #time start and end in millieseconds -> convert to seconds
-audiotexts = []
+audiotexts = [] #list of dictionaires
 
-import numpy as np
+
 count = 0
 for a in diarization:
     data = a
     speaker = data["speaker"]
-    starttime = data["start"]/1000.0 #convert to seconds
-    endtime = data["end"]/1000.0 #convert to seconds
+    starttime = int(data["start"]/1000) #convert to seconds
+    endtime = int(data["end"]/1000) #convert to seconds
     speech = data["text"]
-    duraction = np.arange(starttime,endtime+1,0.001)
+    duraction = list(range(starttime,endtime))
+    #duraction = np.arange(starttime,endtime+1,0.001)
     
     text = {
         "speaker" : speaker,
-        "start" : starttime, 
-        "end" : endtime,
+        # "start" : starttime, 
+        # "end" : endtime,
         "duration" : duraction,
         "speech" : speech,
         "index" : count,
     }
     count += 1
     audiotexts.append(text)
+
+print(audiotexts)
 
